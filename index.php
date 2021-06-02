d361d0155384dc7a0aa70a72ef03b4eb0d2a49b2
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-02 12:36:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -17,35 +17,14 @@
 	<div class="header">
 		<h2>Home Page</h2>
 	</div>
-	<div class="content">
-		<!-- notification message -->
-		<?php if (isset($_SESSION['success'])) : ?>
-			<div class="error success" >
-				<h3>
-					<?php 
-						echo $_SESSION['success']; 
-						unset($_SESSION['success']);
-					?>
-				</h3>
-			</div>
-		<?php endif ?>
-		<!-- logged in user information -->
-		<div class="profile_info">
-			<img src="images/user_profile.png">
+	<p class="Introduction">
+    You will will be able to select the most popular song(s)
+    and or artist of the day, of the week, of the month and of the year, 
+     but you have to register first.
+</p>
+<a href="User Log  In.html">Click here to register</a>
+<p class="registered">Already registered?</p>
+	<a href="#">Click here to log in</a>
 
-			<div>
-				<?php  if (isset($_SESSION['user'])) : ?>
-					<strong><?php echo $_SESSION['user']['username']; ?></strong>
-
-					<small>
-						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
-						<br>
-						<a href="index.php?logout='1'" style="color: red;">logout</a>
-					</small>
-
-				<?php endif ?>
-			</div>
-		</div>
-	</div>
 </body>
 </html>
