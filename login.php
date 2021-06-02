0e9f4857f006e156c3d2fb3b2753c005730e0f7c
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-02 12:27:58 -0400

    Update login.php

--- login.php
+++ login.php
@@ -23,10 +23,27 @@
 		<div class="input-group">
 			<label>Username</label>
 			<input type="text" name="username" >
+			if(uname.value.trim() =="")
+            {
+                alert("blank Username");
+                uname.style.border="solid 3px red";
+                return false;
+            }
 		</div>
 		<div class="input-group">
 			<label>Password</label>
 			<input type="password" name="password">
+			if(password.value.trim()==""){
+                		alert("blank password");
+                		return false;
+            		}
+           		else if(password.value.trim().length<5){
+                		alert("password too short");
+                		return false;
+            		}
+            		else{
+                		return true;
+            		}	
 		</div>
 		<div class="input-group">
 			<button type="submit" class="btn" name="login_btn">Login</button>
