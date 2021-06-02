590487788a19ad5bc69811076fa4fa8620ff2b58
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-02 12:40:47 -0400

    Update login.php

--- login.php
+++ login.php
@@ -4,6 +4,34 @@
 <head>
 	<title>Log In</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript">
+	function validate()
+        {
+            var uname = document.getElementById("uname");
+            var password = document.getElementById("pass");
+            if(uname.value.trim() =="")
+            {
+                alert("blank Username");
+                uname.style.border="solid 3px red";
+                return false;
+            }
+            else if(password.value.trim()=="")
+            {
+                alert("blank password");
+                return false;
+            }
+            else if(password.value.trim().length<5)
+            {
+                alert("password too short");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+        </script>
 </head>
 <body>
 	<div id="logo">
@@ -23,27 +51,10 @@
 		<div class="input-group">
 			<label>Username</label>
 			<input type="text" name="username" >
-			if(uname.value.trim() =="")
-            {
-                alert("blank Username");
-                uname.style.border="solid 3px red";
-                return false;
-            }
 		</div>
 		<div class="input-group">
 			<label>Password</label>
-			<input type="password" name="password">
-			if(password.value.trim()==""){
-                		alert("blank password");
-                		return false;
-            		}
-           		else if(password.value.trim().length<5){
-                		alert("password too short");
-                		return false;
-            		}
-            		else{
-                		return true;
-            		}	
+			<input type="password" name="password">	
 		</div>
 		<div class="input-group">
 			<button type="submit" class="btn" name="login_btn">Login</button>
