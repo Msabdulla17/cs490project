fcbd1064dbdfa33a18ad4359ff93f92846c25f31
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-02 12:57:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -22,9 +22,9 @@
     and or artist of the day, of the week, of the month and of the year, 
      but you have to register first.
 </p>
-<a href="User Log In.html">Click here to register</a>
+<a href="register.php">Click here to register</a>
 <p class="Registered">Already registered?</p>
-	<a href="#">Click here to log in</a>
+	<a href="login.php">Click here to log in</a>
 
 </body>
 </html>
