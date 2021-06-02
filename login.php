4d3601444aec80d894c94fe6139e69e5291bbbdd
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-02 12:49:05 -0400

    Update login.php

--- login.php
+++ login.php
@@ -45,11 +45,11 @@
 
 		<div class="input-group">
 			<label>Username</label>
-			<input type="text" name="username" >
+			<input type="uname" name="Username" >
 		</div>
 		<div class="input-group">
 			<label>Password</label>
-			<input type="password" name="password">	
+			<input type="pass" name="Password">	
 		</div>
 		<div class="input-group">
 			<button type="submit" class="btn" name="login_btn">Login</button>
