db82ef9611e6dbc9b897a49c84ce019e501338f9
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-02 16:34:05 -0400

    Update login.php

--- login.php
+++ login.php
@@ -5,28 +5,6 @@
 	<title>Log In</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript">
-	function validate(){
-            var uname = document.getElementById("uname");
-            var password = document.getElementById("pass");
-            if(uname.value.trim() ==""){
-                alert("blank Username");
-                uname.style.border="solid 3px red";
-                return false;
-            }
-            else if(password.value.trim()==""){
-                alert("blank password");
-                return false;
-            }
-            else if(password.value.trim().length<5){
-                alert("password too short");
-                return false;
-            }
-            else{
-                return true;
-            }
-            
-        }
-        </script>
 </head>
 <body>
 	<div id="logo">
@@ -45,11 +23,11 @@
 
 		<div class="input-group">
 			<label>Username</label>
-			<input type="uname" name="Username" >
+			<input type="text" name="user" >
 		</div>
 		<div class="input-group">
 			<label>Password</label>
-			<input type="pass" name="Password">	
+			<input type="pass" name="password">	
 		</div>
 		<div class="input-group">
 			<button type="submit" class="btn" name="login_btn">Login</button>
