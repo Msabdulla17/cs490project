b437c951fc55983c7a430620db4c5f92fc47d230
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-02 16:23:17 -0400

    Update login.php

--- login.php
+++ login.php
@@ -4,6 +4,13 @@
 <head>
 	<title>Log In</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript">
+        function validate()
+        {
+            var uname = document.getElementById("uname");
+            var password = document.getElementById("pass");
+	}
+	</script>
 </head>
 <body>
 	<div id="logo">
@@ -22,11 +29,11 @@
 		
 		<div class="input-group">
 			<label>Username</label>
-			<input type="text" name="user">
+			<input type="uname" name="user">
 		</div>
 		<div class="input-group">
 			<label>Password</label>
-			<input type="password" name="password">	
+			<input type="pass" name="password">	
 		</div>
 		<div class="input-group">
 			<button type="submit" class="btn" name="login_btn">Login</button>
