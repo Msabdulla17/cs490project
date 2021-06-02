1bced7e7484f74035fc21b783950063cabc9aff6
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-02 07:58:23 -0400

    Update login.php

--- login.php
+++ login.php
@@ -6,6 +6,8 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+	<cancas id="canvas1"></canvas>
+	<script src="script.js"></script>
 	<div id="logo">
 		<br>
 		<div style ="font-size: 35px;">Title for website</div>
