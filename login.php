58fecde8ffc554f4638e3d64a706267283875744
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-03 06:59:15 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,29 @@
-<?php include('functions.php') ?>
+<?php
+$database = mysqli_connect("localhost","root","","users");//Connect to database
+
+if (mysqli_connect_errno()) // Check connection
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+if (isset($_REQUEST['submit']))// If form submitted, insert values into the database.
+{
+  
+$name = stripslashes($_REQUEST['name']);// removes backslashes
+$name = mysqli_real_escape_string($database,$name); //escapes special characters in a string
+$email = stripslashes($_REQUEST['email']);
+$email = mysqli_real_escape_string($database,$email);
+$password = stripslashes($_REQUEST['password']);
+$password = mysqli_real_escape_string($database,$password);
+
+$query = "INSERT into `user` (name, email, password) VALUES ('$name','$email', '".md5($password)."')";
+$result = mysqli_query($database,$query);
+if($result){
+echo "<h3>You are registered successfully.</h3>
+<br/>Click here to <a href='login.php'>Login</a>";
+}
+}else{
+?>
 <!DOCTYPE html>
 <html>
 <head>
