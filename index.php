b403bd392d4eacd55b583cab8b5ce02c3d1fcdba
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-06-08 19:00:10 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -80,14 +80,13 @@
 		<div style="display: flex;">
 		<!-- feed below cover photo and profile picture -->
 			<!--friends--> 
-			<div style= "color:#b1424d; min-height: 400px; flex: 1;">
+			<div style= "background-color: white; color:#b1424d; min-height: 400px; flex: 1;">
 				<div id="friends_bar">
 					Friends
 					<br>
 					<?php
 						if($all_friends)
 						{
-							var_dump($all_friends);
 							foreach ($all_friends as $FRIEND_ROW)
 							{
 								include('user.php');
