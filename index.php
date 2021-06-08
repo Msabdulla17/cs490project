e2c67e79f3f5e518e1382972b28ea4b6f156f9e2
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-06-08 11:23:21 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 	if (isset($_POST['post_btn'])) 
 	{
 		create_post();
-		header('location: https://cs490summerproject.herokuapp.com/index.php');
+		header('location: login.php');
 		exit();
 	}
 ?>
