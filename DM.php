35d0d38eef6d9e2b5165be5ff72bde9f3b7d794c
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-09 07:15:43 -0400

    Create DM.php

---
+++ DM.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DMs</title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+ <div style="width: 800px; margin: auto; min-height: 400px;">
+	<div style="display: flex;">
+		<!-- feed below cover photo and profile picture -->
+			<!--friends--> 
+			<div style= "background-color: white; color:#b1424d; min-height: 400px; flex: 1;">
+				<div id="friends_bar">
+					Friends
+					<br>
+					<?php
+						if($all_friends)
+						{
+							foreach ($all_friends as $FRIEND_ROW)
+							{
+								include('user.php');
+							}
+						}
+					?>
+
+				</div>
+			</div>
