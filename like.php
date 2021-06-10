fd4ea9e7c481a56287635126a108b3fea492dc87
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-06-09 20:52:11 -0400

    Add files via upload

--- like.php
+++ like.php
@@ -1,23 +1,29 @@
 <?php
-
-    echo "<pre";
-    echo ($_GET);
-    echo "<pre";
+    
+    if(isset($_SERVER['HTTP_REFERER']))
+    {
+       $return_to = $_SERVER['HTTP_REFERER'];
+    }
+    else
+    {
+       $return_to = "index.php";
+    }
 
     if(isset($_GET['like_type']) && isset($_GET['post_id']))
     {
-        if (is_numeric($_GET['id']))
+        if (is_numeric($_GET['post_id']))
         {
            $allowed[] = 'post';
            $allowed[] = 'user';
            $allowed[] = 'comment';
 
-            if (in_array($_GET['type'], $allowed))
+            if (in_array($_GET['like_type'], $allowed))
             {
                 like_post($_GET['post_id'], $_GET['type']);
             }
         }
     }
+    header("location: " . $return_to);
     die;
 ?>
 
