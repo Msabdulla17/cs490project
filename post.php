dfab1beb60bda04d0d3c417ea847e49d586010d8
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-06-10 17:05:05 -0400

    Add files via upload

--- post.php
+++ post.php
@@ -4,10 +4,12 @@
 	</div>
 	<div>
 	    <div style="font-weight: bold; color: #b1424d;">
-            <?php 
+            <?php
+            echo "<a href='index.php?id=$ROW[users_id]'>"; 
             echo $ROW_USER["first_name"];
             echo " ";
             echo $ROW_USER["last_name"]; 
+            echo "</a>"
             ?>
         </div>
         <?php echo $ROW['post']; ?>
