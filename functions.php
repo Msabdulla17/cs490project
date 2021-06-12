ef3d3c9691d09dbd69aed46d924f9ca634441f92
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-06-11 23:20:11 -0400

    Add files via upload

--- functions.php
+++ functions.php
@@ -45,25 +45,26 @@ function like_post($id, $like_type)
 		$query = "SELECT likes FROM likes
 				WHERE like_type = 'post' && content_id = '$id' LIMIT 1";
 		$result = mysqli_query($db, $query);
-		foreach ($result as $results)
+
+		if(!is_array($result))
 		{
-			if(!is_array($results))
-			{
-				$query = "UPDATE posts SET likes = likes + 1 
-						WHERE post_id = '$id' LIMIT 1";
-				mysqli_query($db, $query);
+			$query = "UPDATE posts SET likes = likes + 1 
+					WHERE post_id = '$id' LIMIT 1";
+			mysqli_query($db, $query);
 
-				$arr["user_id"] = $user_id;
-				$arr["date"] = date("Y-m-d H:i:s");
-				$arr2[] = $arr;
-				$likes_string = json_encode($arr2);
+			$arr["user_id"] = $user_id;
+			$arr["date"] = date("Y-m-d H:i:s");
+			$arr2[] = $arr;
+			$likes_string = json_encode($arr2);
 
-				$query = "INSERT INTO likes (like_type, content_id, likes)
-							VALUES ('$like_type','$id', '$likes_string')";
-				mysqli_query($db, $query);
-				
-			}
-			else
+			$query = "INSERT INTO likes (like_type, content_id, likes)
+						VALUES ('$like_type','$id', '$likes_string')";
+			mysqli_query($db, $query);
+			
+		}
+		else
+		{
+			foreach ($result as $results)
 			{
 				$likes = json_decode($results['likes'],true);
 				$liker_user_ids = array_column($likes, "user_id");
@@ -90,7 +91,6 @@ function like_post($id, $like_type)
 		}
 	}
 	exit();
-
 }	
 
 function create_random_id()
