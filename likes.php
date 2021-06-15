6895f98e5db55e3b7bd2ee1a55cf8c8e9724f752
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-06-15 18:48:35 -0400

    Add files via upload

--- likes.php
+++ likes.php
@@ -50,6 +50,7 @@
                                 {
                                     foreach ($likes as $ROW)
                                     {
+                                        var_dump($ROW);
                                         $FRIEND_ROW = getUserByID($ROW['user_id']);
                                         include("user.php");
                                     }
