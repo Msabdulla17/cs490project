9fe72253aaf7d808fa62dd6385442fded66ceefb
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-06-15 17:05:25 -0400

    Add files via upload

--- functions.php
+++ functions.php
@@ -212,11 +212,8 @@ function get_likes($id, $like_type)
 		$result = read($query);
 		if(is_array($result))
 		{
-			foreach ($result as $results)
-			{
-				$likes[] = json_decode($results['likes'],true);
-			}
-				return $likes;
+			$likes = json_decode($result[0]['likes'],true);
+			return $likes;
 		}
 		else
 		{
