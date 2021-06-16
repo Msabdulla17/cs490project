0b1846de81441b2c2efa7da697882bc21382389b
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-16 15:55:33 -0400

    Update single_post.php

--- single_post.php
+++ single_post.php
@@ -95,7 +95,13 @@
 						<br>
 					</form>
 				</div>
-                
+				<div>
+					<form action="upload.php" method="post" enctype="multipart/form-data">
+						Select image to upload:
+						<input type="file" name="fileToUpload" id="fileToUpload">
+						<input type="submit" value="Upload Image" name="submit">
+					</form>
+				</div>
                 <?php
                    
                    $comments = get_comments($post_id);
