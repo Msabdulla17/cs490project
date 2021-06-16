ae6b21c206b8674cef7fa2ce25d556e2e7010c3e
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-16 13:55:30 -0400

    Update messages.php

--- messages.php
+++ messages.php
@@ -49,54 +49,58 @@ if(isset($URL[1] && is_numeric($URL[1]){
 	<link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
-	<form style="width:100%; background-color: #b1424d;" method = "get" action="search.php">	
-		<div id="top_bar">
-			<div style="width: 800px; height: 50px; margin:auto; font-size: 30px;">
-				<a href="timeline.php" style="color: white";>Artstagram</a>
-				&nbsp &nbsp 
-				<input type="text" name="find" id="search_box" placeholder="Search">
-				<a href ="index.php"><img src="images/user_profile.png" style="width: 40px; float: right;"></a>
-				<?php  if (isset($_SESSION['user'])) : ?>
-					<a href="index.php?logout='1'" style="font-size: 11px; float: right; margin: 10px; color: white;">
-					Log Out
-					</a>		
-				<?php endif ?>
-			</div>
-		</div>
-	</form>
-	<!-- Main Body -->
+<br>
+	<?php include("index.php"); ?>
 	<div style="width: 800px; margin: auto; min-height: 400px;">
-	<div style="display: flex;">
-			<!--make a post and recent posts--> 
-			<div style= "min-height: 400px; padding: 20px; padding-right: 0px; flex:2.5;">
-				<div> 
-					<form style= "width: 100%;" method= "post">
-						<hr>
-							<?php
-								if($ROW2)
-								{
-									echo "Are you sure you want to delete this post?<br>";
-									foreach ($ROW2 as $ROW)
-									{
-										$ROW_USER = getUserById($ROW['users_id']);
-										include("post_delete.php");
-									}
-								}
+		<div style="display: flex;">
+		<!-- feed below cover photo and profile picture -->
+		 <div style= "min-height: 400px; flex:2.5;padding: 20px;padding-right: 0px;"> 
+			<div style= "border:solid thin #aaa; background-color: white; color:#b1424d;">
+				<form method="post" enctype="multipart/form-data">
+					<?php
+						if($ERROR != "")
+						{
+							echo $ERROR;
+						}
+						else
+						{
+							if(isset($URL[1]) && $URL[1] == "new"){
+								echo "Start New Message with:<br><br>";
+								if(isset($URL[2]) && is_numeric($URL[2])
+								   {
+									$user = new User();
+									$FRIEND_ROW = $user->get_user($URL[2]);
+									   }
+								include "user.php";
+								   echo '';
+								<div style= "border:solid thin #aaa; padding: 10px; background-color: white; color:#b1424d;">
+									<form method="POST">
+								 	  <textarea name="message" placeholder="Write your message here"></textarea>
+								 	  <input type="file" name="file" multiple>
+								 	  <input id="post_button" type="submit" value="Send">
+								   	</form>
+								   </div>;
 								else
 								{
-									array_push($errors, "No post was found.");
-									header("index.php");
+									echo "That user could not be found<br><br>";
 								}
-							?>
-						<hr>
-						<input type ="hidden" name="post_id" value=<?php echo $post_id ?>>
-						<input id="delete_button" type="submit" name="delete_btn" value="Delete">
-						<br>
-					</form>
-                <br style="clear: both;">
+							}
+							else
+							{
+								echo "Messages<br><br>";
+								$user = new User();
+								$ROW_USER = $user->get_user($ROW['users_id']);
+							}
+							include("message.php");
+							echo "<input type='hidden' name='post_id' value='$ROW[post]'>";
+							echo "<input id='post_button' type=submit value='Delete'>";
+						}
+					?>
+
+				</form>
 				</div>
-			</div>
 		</div>
 	</div>
+</div>
 </body>
 </html>
