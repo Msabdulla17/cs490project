6316d7f628bda7f33e1033feb5dc743050b85c0f
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-06-16 19:20:16 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -89,16 +89,15 @@
 			<div id="menu_buttons"><a href="" style="color:#b1424d;">Friends</a></div>
 			<div id="menu_buttons"><a href="" style="color:#b1424d;">Photos</a></div>
 			<?php if ($profile_data['id'] == $user_id): ?>
-				<a href='messages.php'.$user_id.'/settings'>
-					<input id='menu_buttons' type='button' value='Messages'>
+				<a href='messages.php'>
+				<div id='menu_buttons'>Messages</div>
 				</a>
 				<a href='index'.$user_id.'/settings'>
 					<div id='menu_buttons'>Settings</div>
 				</a>
-
 			<?php else: ?>
-				<a href='message.php'.$user_id.'/settings'>
-					<input id='menu_buttons' type='button' value='Message'>
+				<a href='messages.php'>
+					<div id='menu_buttons'>Message</div>
 				</a>
 			<?php endif; ?>
 		</div>
