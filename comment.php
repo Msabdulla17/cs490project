a38444acf8d795c38ce72aa02da89fd6e3f1d7bb
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-17 14:52:24 -0400

    Update comment.php

--- comment.php
+++ comment.php
@@ -5,24 +5,7 @@
 	<div>
 	    <div style="font-weight: bold; color: #b1424d;">
             <?php
-		    $client = new http\Client;
-		$request = new http\Client\Request;
 
-		$request->setRequestUrl('https://instagram47.p.rapidapi.com/post_comments');
-		$request->setRequestMethod('GET');
-		$request->setQuery(new http\QueryString([
-			'postid' => '2435143128484144113'
-		]));
-
-	$request->setHeaders([
-			'x-rapidapi-key' => 'f57f39d658mshc8cb5b1a871b6e3p1b0d14jsnf0d710d0d955',
-			'x-rapidapi-host' => 'instagram47.p.rapidapi.com'
-	]);
-
-	$client->enqueue($request)->send();
-	$response = $client->getResponse();
-
-	echo $response->getBody();
         	    echo "<a href='index.php?id=$COMMENT[users_id]'>"; 
             	echo $profile_data["first_name"];
             	echo " ";
