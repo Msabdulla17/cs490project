5cc7c930fa1118cce8ef41f1c443128e25b3aaef
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-18 17:25:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,22 @@
-const canvas=document.getElementById("canvas1");
-const ctx=canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-let particlesArray;
-let mouse = {
-  x: null,
-  y: null,
-  radius: (canvas.height/80) * (canvas.width/80);
-}
-window.addEventListener('mousemove',
-                        function(event) {
-                          mouse.x = event.x;
-                          mouse.y = event.y;
-}
-                        );
-class Particle{
-  constructor(x,y,directx,directy, size, color){
-    this.x=x;
-    this.y=y;
-    this.directx=directx;
-    this.directy=directy;
-    this.size=size;
-    this.color=color;
-  }
-  draw(){
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size,0, Math.PI*2, false);
-    ctx.fillStyle= '#0c0880';
-    ctx.fill();
-  }
-  update(){
-    if (this.x>canvas.width||this.x<0){
-      this.directx = -this.directx;
-    }
-    if (this.y>canvas.height||this.y<0){
-      this.directy = -this.directy;
-    }
-    
-  }
-}
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(morgan('common'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+})
