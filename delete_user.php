56408b9662d5dd817f7505927d10c0ae8ccd401d
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-19 19:16:27 -0400

    Update delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -13,3 +13,17 @@
 		exit();
 	}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Delete User</title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<style>
+		.header {
+			background: #003366;
+		}
+		button[name=register_btn] {
+			background: #003366;
+		}
+	</style>
+</head>
