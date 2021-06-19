5543fea5dba013ad62d0d8d94aa6f9a4eb27d1dd
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-18 20:55:12 -0400

    Update curl_api.php

--- curl_api.php
+++ curl_api.php
@@ -30,7 +30,7 @@
   <div id="result"></div>
   </div>
 </body>
-<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
   $("#myForm").submit(function(event){
