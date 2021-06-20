54bd403eca06ee9a8f5e85436fb2cedb1854c3ab
IkeWarrior <84685407+IkeWarrior@users.noreply.github.com>
2021-06-20 11:53:10 -0400

    Update curl_api.php

--- curl_api.php
+++ curl_api.php
@@ -14,7 +14,6 @@
 	<link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
-	<!--top bar-->
 	<form style="width:100%; background-color: #b1424d;" method = "get" action="search.php">	
 		<div id="top_bar">
 			<div style="width: 800px; height: 50px; margin:auto; font-size: 30px;">
@@ -30,49 +29,46 @@
 			</div>
 		</div>
 	</form>
-	<!-- photo search -->
-  <div class="container">
-  <br><br>
-  <h1 style="text-align: center;">Unsplash Photo Search</h1>
-  <form id="myForm" autocomplete="off">
-  <div class="form-group">
-  <input type="text" class="form-control" id="search" placeholder="Search Photos" required>
-  </div>
-  <div class="form-group">
-  <button class="btn btn-danger btn-block">
-  Search Photos
-  </button>
-  </div>
-  </form>
-  <div id="result"></div>
-  </div>
+	<div class="container">
+  	<br><br>
+	<h1 style="text-align: center;">Unsplash Photo Search</h1>
+  	<form id="myForm" autocomplete="off">
+  		<div class="form-group">
+  			<input type="text" class="form-control" id="search" placeholder="Search Photos" required>
+  		</div>
+  		<div class="form-group">
+ 	 		<button class="btn btn-danger btn-block">
+  				Search Photos
+  			</button>
+  		</div>
+  	</form>
+  	<div id="result"></div>
+  	</div>
 </body>
-	<!-- api link -->
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
-  $("#myForm").submit(function(event){
-  event.preventDefault()
-	  $("#result").empty()
-  var search = $("#search").val()
- <!-- my api required -->
-  var url = "https://api.unsplash.com/search/photos?query="+search+"&client_id=D0zuSiTKvrj8GHZG91rRLSNLu20jmitBUDeS2D1EQCg&per_page=50"
-  $.ajax({
-  method:'GET',
-  url:url,
-  success:function(data){
-  console.log(data)
-    data.results.forEach(photo => {
-      $("#result").append(`
-      <h2 class="name">${photo.user.name}</h2>
-      <h3 class="link">
-	<a href="${photo.user.links.html}">${photo.user.username}</a>
-	</h3>
-      <img src="${photo.urls.regular}"/>
-      `)
-    });
-  }
-  })
-  })
+	$("#myForm").submit(function(event){
+  		event.preventDefault()
+	  	$("#result").empty()
+  		var search = $("#search").val()
+  		var url = "https://api.unsplash.com/search/photos?query="+search+"&client_id=D0zuSiTKvrj8GHZG91rRLSNLu20jmitBUDeS2D1EQCg&per_page=50"
+  		$.ajax({
+  			method:'GET',
+  			url:url,
+  			success:function(data){
+  				console.log(data)
+    				data.results.forEach(photo => {
+      					$("#result").append(`
+      					<h2 class="name">${photo.user.name}</h2>
+      					<h3 class="link">
+						<a href="${photo.user.links.html}">${photo.user.username}</a>
+					</h3>
+      					<img src="${photo.urls.regular}"/>
+      					`)
+    				});
+  			}
+  		})
+  	})
 </script>
 </html>
