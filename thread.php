b150b8662f577c9c59aaa9f63020e0b7dc65ae7f
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-06-23 22:44:20 -0400

    Add files via upload

--- thread.php
+++ thread.php
@@ -1,4 +1,4 @@
-<div id="message_thread" style="background-color: #eee">
+<div id="message_thread" style="background-color: #eee; position:relative;">
 	<div>
         <?php
             $thumb_image = "images/user_profile.png";
@@ -27,6 +27,13 @@
 		<span style="color: #b1424d;">
             <?php echo ($single_thread['date']); ?>
         </span>
+        <a href="messages.php?type=read&user_id=<?php echo $message_owner['id']?>">
+            <div style="cursor: pointer; background-color:#b1424d; height:100%; width:50px; position:absolute; right:10px; top:4px;">
+                <svg style="position:absolute; left:50%; top:50%; transform:translate(-50%,-50%)" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                    <path d="M24 23h-24v-13.275l2-1.455v-7.27h20v7.272l2 1.453v13.275zm-20-10.472v-9.528h16v9.527l-8 5.473-8-5.472zm14-.528h-12v-1h12v1zm0-3v1h-12v-1h12zm-7-1h-5v-3h5v3zm7 0h-6v-1h6v1zm0-2h-6v-1h6v1z"/>
+                </svg>
+            </div>
+        </a>
 	</div>
 </div>
 <br><br>
