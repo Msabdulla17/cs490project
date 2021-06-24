9af21612821aa209c9e5bddfde722d698f15c4d0
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-06-23 20:34:44 -0400

    Add files via upload

--- message_left.php
+++ message_left.php
@@ -1,11 +1,20 @@
+<?php
+    $message_owner = getUserById($msg_row['sender']);
+?>
 <div id="message_left" style="background-color: #eee">
 	<div>
-		<img src="images/user_profile.png" style="width: 75px; margin-right: 4px;">
+        <?php
+            $thumb_image = "images/user_profile.png";
+            if (file_exists($message_owner['profile_image']))
+            {
+                $thumb_image = $message_owner['profile_image'];
+            }
+        ?>
+		<img src="<?php echo $thumb_image ?>" style="width: 75px; margin-right: 4px;">
 	</div>
 	<div>
 	    <div style="font-weight: bold; color: #b1424d;">
             <?php
-            $message_owner = getUserById($msg_row['sender']);
             echo "<a href='index.php?id=$msg_row[sender]'>"; 
             echo $message_owner["first_name"];
             echo " ";
@@ -14,6 +23,9 @@
             ?>
         </div>
         <?php echo $msg_row['message']; ?>
+        <?php  if (file_exists($msg_row['file'])) : ?>
+            <img src = "<?php echo $msg_row['file'] ?>" style="width:200px;">
+        <?php endif ?>
         <br><br>
 		<span style="color: #b1424d;">
             <?php echo ($msg_row['date']); ?>
